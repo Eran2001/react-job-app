f4f6a9cf8264bb425adfa9e258ea2c5def16622a
Eran <eran.hasarali.2001@gmail.com>
2025-01-24 20:27:08 +0530

    make some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Navbar from "./components/Navbar/Navbar"
 
 
 const App = () => {
   return (
     <div>
-      <header className="">
-        <h1>Hello</h1>
-      </header>
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>LOGO</h1>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
