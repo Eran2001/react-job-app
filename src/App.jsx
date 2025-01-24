a5ca79b18971ba5edf9ea71a8faedd9651f2cc01
Eran <eran.hasarali.2001@gmail.com>
2025-01-24 21:30:18 +0530

    style Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Navbar from "./components/Navbar/Navbar"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <h1>LOGO</h1>
+      <h1><Link to="/">LOGO</Link></h1>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><button className="btn"><Link to="/contact">Contact</Link></button></li>
       </ul>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
